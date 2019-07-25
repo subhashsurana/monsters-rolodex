0651f146b4b98285e28bc94a35ae189f0b917a46
Subhash Surana <Subhash.surana23@gmail.com>
2019-07-25 11:59:52 +0800

    Updates

---
+++ precache-manifest.34438609d7fe582201d937cb82ba78de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0a1989805363aae105399cab84a7b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eeed9ad5ac153f8d877b",
+    "url": "/monsters-rolodex/static/css/main.56813240.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "eeed9ad5ac153f8d877b",
+    "url": "/monsters-rolodex/static/js/main.5ad60a8d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
